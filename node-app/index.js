c356488c89e7e0de8b7737df8d61f30df42e61a8
Fukubi <deividgabrielproc@gmail.com>
2020-11-14 16:53:16 -0300

    feat: chat filtering added in bot

--- node-app/index.js
+++ node-app/index.js
@@ -21,22 +21,40 @@ app.post('/webhook', async function (req, res) {
         const body = data.messages[i].body;
         const chatId = data.messages[i].chatId;
         const senderName = data.messages[i].senderName;
-
+        let save= {};
         if(data.messages[i].fromMe)return;
 
         console.log(data);
         
-        if(/1/.test(body)){
-            const text = `Digite seu CPF e mande sua localização para enviarmos uma equipe`
+        if(/1/.test(body) && save[`${chatId}`] === 'choice'){
+            const text = `Por favor informe o CPF ou CNPJ do responsável`;
             await apiChatApi('sendMessage', {chatId: chatId, body: text});
-        } else if(/2/.test(body)) {
-            const text = `Digite seu CPF e mande sua localização para enviarmos uma equipe`
+            save[`${chatId}`] = 'choice1'
+        } else if(/2/.test(body) && save[`${chatId}`] === 'choice') {
+            const text = `Digite seu CPF e mande sua localização para enviarmos uma equipe`;
+            await apiChatApi('sendMessage', {chatId: chatId, body: text});
+            save[`${chatId}`] = 'choice2'
+        } else if (/05432246945/.test(body) && save[`${chatId}`] === 'choice1') {
+            const text = `Olá Daniel Augusto Krug! Envie sua localização para enviarmos uma equipe para o local`;
+            await apiChatApi('sendMessage', {chatId: chatId, body: text});
+
+            const text = `A equipe irá chegar no local com um tempo mínimo de 3 horas`;
+            await apiChatApi('sendMessage', {chatId: chatId, body: text});
+        } else if (/12345678912/.test(body) && save[`${chatId}`] === 'choice1') {
+            const text = `Olá Daniel Augusto Krug! Envie sua localização para enviarmos uma equipe para o local`;
+            await apiChatApi('sendMessage', {chatId: chatId, body: text});
+
+            const text = `A equipe irá chegar no local com um tempo mínimo de 3 horas`;
             await apiChatApi('sendMessage', {chatId: chatId, body: text});
         } else {
-            const text = `Oi, eu sou a Joana, assistente virtual da CPFL. Estou aqui pra te ajudar. Por favor digite o número do serviço que deseja solicitar.
+            if(!save[`${chatId}`]) {
+                const text = `Oi, eu sou a Joana, assistente virtual da CPFL. Estou aqui pra te ajudar. Por favor digite o número do serviço que deseja solicitar.
 1. Queda de energia elétrica 
 2. Religação de energia`;
-            await apiChatApi('sendMessage', {chatId: chatId, body: text});
+                await apiChatApi('sendMessage', {chatId: chatId, body: text});
+            }
+
+            save[`${chatId}`] = 'choice';
         }
     }
     res.send('Ok');
